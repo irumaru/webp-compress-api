5ac1e465f20c49dd76f8bbf226be07c7d6ca0335
Irumaru <46730701+irumaru@users.noreply.github.com>
2025-12-20 13:19:20 +0900

    テストシナリオをテストに使用したものに変更 (#4)

--- test/loadtest.js
+++ test/loadtest.js
@@ -1,4 +1,4 @@
-// k6 run test/loadtest.js
+// k6 run test/loadtest.js --out=web-dashboard
 // or
 // curl -X POST -F image=@./test/testdata/1.4MB.jpg -o test/testdata/out.webp http://127.0.0.1:1323/v1/compress
 
@@ -7,18 +7,18 @@ import { sleep, check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 }, // 最初の30秒でVUsを0から20まで増加
-    //{ duration: '1m', target: 10 },  // 次の1分間でVUsを20から10まで減少
-    //{ duration: '30s', target: 0 },  // 最後の30秒でVUsを10から0まで減少
+    { duration: '15m', target: 15 },
+    { duration: '10m', target: 15 },
+    { duration: '15m', target: 0 },
   ],
 };
 
-const fileData = open('test/testdata/1.4MB.jpg', 'b'); // 実際のファイルパスに変更
+const fileData = open('testdata/1.4MB.jpg', 'b'); // 実際のファイルパスに変更
 const form = {
   image: http.file(fileData, 'test-image.jpg', 'image/jpeg'),
 };
 
 export default function() {
-  let res = http.post('http://127.0.0.1:1323/v1/compress', form);
+  let res = http.post('http://127.0.0.1.nip.io/v1/compress', form);
   check(res, { "status is 200": (res) => res.status === 200 });
 }
